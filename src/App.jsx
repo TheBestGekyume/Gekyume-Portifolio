a8a92ff29356837b6ad33bcd040691e23f6cefca
Gekyume <contatogekyume@gmail.com>
2024-07-31 12:10:38 -0300

    header-responsivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,69 @@
-import './App.css';
-import { Header } from './Components/Header/Header';
-import { Navbar } from './Components/Navbar/Navbar';
-import { Cards } from './Components/Cards/Cards';
+import "./App.css";
+import { Header } from "./Components/Header/Header";
+import { Navbar } from "./Components/Navbar/Navbar";
+import { Cards } from "./Components/Cards/Cards";
 
 export function App() {
   return (
     <div className="App">
-      
-    <Navbar/>
+      <Navbar />
 
-    <Header/>
+      <Header />
 
-    {/*  FAZER OS CARD COM BACKGROUND IMAGEM COM OPACIDADE BAIXA E AO CLICAR APARECER MODAL */}
+      {/*  FAZER OS CARD COM BACKGROUND IMAGEM COM OPACIDADE BAIXA E AO CLICAR APARECER MODAL */}
 
-    <div class="row row-cols-1 row-cols-md-4">
-      <Cards/>
-    </div>
+      <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+
+        <div class="carousel-inner">
+          <Cards 
+            imageSrc="https://thebestgekyume.github.io/Rainy-Night/img/headerImg.png"
+           bgColor="#000" 
+           link="https://thebestgekyume.github.io/Rainy-Night/" 
+           description="tomi" />
+        </div>
+
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
   );
 }
-
--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,62 +1,17 @@
 import "./Cards.css";
 
-export function Cards() {
+export function Cards({ imageSrc, bgColor, link, description }) {
   return (
-      <>
-      <div class="col">
-        <div class="card">
+    <>
+      <div class="carousel-item active">
+        <a href={link} target="_blank">
           <img
-            src="https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png"
-            class="card-img-top"
-            alt="..."
+            src={imageSrc}
+            class="d-block w-100"
+            alt={description}
+            style={{ backgroundColor: bgColor }}
           />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card witgh supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
+        </a>
       </div>
     </>
   );
