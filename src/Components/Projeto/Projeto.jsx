bcd786ad75a2ff1e8fd6097009006348fdbeaea0
Gekyume <contatogekyume@gmail.com>
2024-09-22 16:09:53 -0300

    Redefinição do layout dos projetos

--- src/Components/Projeto/Projeto.jsx
+++ src/Components/Projeto/Projeto.jsx
@@ -1,19 +1,17 @@
 import "./Projeto.css";
 
-export function Projeto({ imageSrc, title, link, description, bgColor }) {
+export function Projeto({ imageSrc, title, link, description}) {
   return (
     <a href={link} target="_blank">
-      <div className="col">
-        <div className="card text-bg-dark text-decoration-none">
+        <div className="card pt-2 pt-md-0 text-bg-dark text-decoration-none">
         <h5 className="card-title d-block d-md-none text-center fw-bold">{title}</h5>
 
           <img src={imageSrc} className="card-img" alt="..." />
 
           <div className="overlay">
             <h5 className="d-none d-md-block card-title text-center fw-bold">{title}</h5>
-            <p className="card-text text-break p-1">{description}</p>
+            <p className="card-text text-break p-0">{description}</p>
           </div>
-        </div>
       </div>
     </a>
   );
