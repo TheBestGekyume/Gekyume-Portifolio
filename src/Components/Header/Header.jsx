f0adcc5666f0fbaa618d57b239036b46aab1ffd0
Gekyume Serna <contatogekyume@gmail.com>
2025-01-21 21:22:57 -0300

    implementa scss

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import "./Header.css";
+import "./Header.scss";
 import React from "react";
 import Particulas from "../Particulas/Particulas";
 
 export function Header({ children }) {
   return (
-    <section id="header">
+    <section id="header" className="d-flex justify-content-center align-items-center h-100 text-white">
       <Particulas id="particulas"/>
-      <div className="div-content">
+      <div className="div-content py-5 px-3">
         <h2>Boa-Vindas ao Meu Portifólio!</h2>
 
         <h3>
@@ -14,16 +14,16 @@ export function Header({ children }) {
         </h3>
 
         <p>
-          Olá! Meu nome é <span>Gekyume</span>, sou estudante de
+          Olá! Meu nome é<span> Gekyume</span>, sou estudante de
           <span> Análise e Desenvolvimento de Sistemas </span>e utilizo
-          principalmente <span> React.js </span>para estruturar os meus projetos
-          Front-end.
+          principalmente<span> React.js </span> e <span> Angular </span>para 
+          estruturar os meus projetos Front-end.
           Tenho conhecimento sólido e experiencia em
-          <span>Lógica de Programação</span>, <span>C</span>,<span> HTML5</span>
+          <span> Lógica de Programação</span>, <span>C</span>,<span> HTML5</span>
           ,<span> CSS3</span> e <span>JavaScript</span>.
         </p>
 
-        <div className="svg-group">{children}</div>
+        <div className="text-center mx-auto svg-content">{children}</div>
       </div>
       
     </section>
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import { App } from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // Remover ou ajustar conforme necessário
 // import reportWebVitals from './reportWebVitals';
 
