351477023fa6dcce989e199229e199f683c78aad
Gekyume Serna <contatogekyume@gmail.com>
2025-04-13 01:28:58 -0300

    implementa deploy para o gh pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base:"/Gekyume-Portifolio/",
   plugins: [react()],
   css:{
     preprocessorOptions:{
